98870751fb577f7bd400cc79bc9f18fe4e9c1adc
Anupama Sharma <20889654+sharmaranupama@users.noreply.github.com>
2023-06-05 22:18:23 +0530

    Update changelog.sql

--- src/main/resources/sql/changelog.sql
+++ src/main/resources/sql/changelog.sql
@@ -7,3 +7,9 @@ CREATE TABLE example (
 );
 --rollback drop table your_table;
 
+--changeset anupama:002_create_table
+CREATE TABLE example1 (
+  id serial PRIMARY KEY,
+  name varchar(255) NOT NULL
+);
+
